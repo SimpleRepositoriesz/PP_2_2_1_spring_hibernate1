0c8fa8d48f4f895afcee33a9c2258747af827154
MegaComp <adilkhanova.zu15@mail.ru>
2025-02-08 19:36:35 +0300

    Hibernate и ORM.

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,12 +1,13 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.persistence.NoResultException;
 import java.sql.SQLException;
-import java.util.List;
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
@@ -15,18 +16,34 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User vasya = new User("Vasya", "Vasechkin", "vasechkin@mail.io");
+      User petya = new User("Petya", "Sidorov", "sidorov@mail.io");
+      User olga = new User("Olga", "Petrova", "petrova@mail.io");
+      User sveta = new User("Svetlana", "Ivanova", "ivanova@mail.io");
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+      Car volvo = new Car("Volvo", 9);
+      Car bmw = new Car("BMW", 325);
+      Car suzuki = new Car("Sisuki", 4);
+      Car lada = new Car("Ladaa", 21014);
+
+      userService.add(vasya.setCar(volvo).setUser(vasya));
+      userService.add(petya.setCar(bmw).setUser(petya));
+      userService.add(olga.setCar(suzuki).setUser(olga));
+      userService.add(sveta.setCar(lada).setUser(sveta));
+
+      // пользователи с машинами
+      for (User user : userService.listUsers()) {
+         System.out.println(user + " " + user.getCar());
+      }
+
+      // достать юзера, владеющего машиной по ее модели и серии
+      System.out.println(userService.getUserByCar("BMW", 325));
+
+      // нет такого юзера с такой машиной
+      try {
+         User notFoundUser = userService.getUserByCar("GAZ", 4211);
+      } catch (NoResultException e) {
+         System.out.println("User not found");
       }
 
       context.close();
